2b5538b1ed37a45114c284b60f08053eaf43538c
Dinara Idrissova <Dinara.Idrissova@list.ru>
2023-10-09 14:02:00 -0400

    display 3d Shirt

---
+++ client/src/canvas/CameroRig.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const CameroRig = () => {
+  return <div>CameroRig</div>;
+};
+
+export default CameroRig;
--- client/src/canvas/index.jsx
+++ client/src/canvas/index.jsx
@@ -1,5 +1,23 @@
-const Canvas = () => {
-  return <div> Canvas</div>;
+import { Canvas } from "@react-three/fiber";
+import { Environment, Center } from "@react-three/drei";
+import Backdrop from "./Backdrop";
+import CameroRig from "./CameroRig";
+import Shirt from "./Shirt";
+
+const CanvasModel = () => {
+  return (
+    <Canvas>
+      {" "}
+      <ambientLight intensity={0.5} />
+      <Environment preset="city" />
+      {/* <CameroRig>
+        <Backdrop /> */}
+      <Center>
+        <Shirt />
+      </Center>
+      {/* </CameroRig> */}
+    </Canvas>
+  );
 };
 
-export default Canvas;
+export default CanvasModel;
