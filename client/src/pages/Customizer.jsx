5954082353b2ef83dd1685e407e61a309c15d5c1
Dinara Idrissova <Dinara.Idrissova@list.ru>
2023-10-10 12:48:56 -0400

    setup Tab Component functionality

--- client/src/pages/Customizer.jsx
+++ client/src/pages/Customizer.jsx
@@ -20,8 +20,33 @@ import {
   Tab,
   CustomBtn,
 } from "../components";
+
 const Customizer = () => {
   const snap = useSnapshot(state);
+
+  //show tab content depending on the active activeTab
+  const [file, setFile] = useState("");
+  const [prompt, setPrompt] = useState("");
+  const [generatingImg, setGeneratingImg] = useState(false);
+  const [activeEditorTab, setActiveEditorTab] = useState("");
+  const [activeFilterTab, setActiveFilterTab] = useState({
+    logoShirt: true,
+    stylishShirt: false,
+  });
+
+  const generateTabContent = () => {
+    switch (activeEditorTab) {
+      case "colorpicker":
+        return <ColorPicker />;
+      case "filepicker":
+        return <FilePicker />;
+      case "aipicker":
+        return <AIPicker />;
+      default:
+        return null;
+    }
+  };
+
   return (
     <AnimatePresence>
       {!snap.intro && (
@@ -35,8 +60,15 @@ const Customizer = () => {
               <div className="flex items-center min-h-screen">
                 <div className="editortabs-container tabs">
                   {EditorTabs.map((tab) => (
-                    <Tab key={tab.name} tab={tab} handleClick={() => {}} />
+                    <Tab
+                      key={tab.name}
+                      tab={tab}
+                      handleClick={() => {
+                        setActiveEditorTab(tab.name);
+                      }}
+                    />
                   ))}
+                  {generateTabContent()}
                 </div>
               </div>
             </motion.div>
