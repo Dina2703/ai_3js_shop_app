8f74268db4bcd49f4d67ed15833f428c3c9d084b
Dinara Idrissova <Dinara.Idrissova@list.ru>
2023-10-12 15:23:40 -0400

    AIPicker doesn't work, getting syntax error

--- client/src/components/AIPicker.jsx
+++ client/src/components/AIPicker.jsx
@@ -1,7 +1,43 @@
 import React from "react";
+import CustomBtn from "./CustomBtn";
 
-const AIPicker = () => {
-  return <div>AIPicker</div>;
+const AIPicker = ({ prompt, setPrompt, generatingImg, handleSubmit }) => {
+  return (
+    <div className="aipicker-container">
+      <textarea
+        className="aipicker-textarea"
+        placeholder="Ask AI..."
+        value={prompt}
+        onChange={(e) => setPrompt(e.target.value)}
+        rows={5}
+      />
+      <div className="flex flex-wrap gap-3">
+        {" "}
+        {generatingImg ? (
+          <CustomBtn
+            type="outline"
+            title="Asking AI..."
+            customStyles="text-xs"
+          />
+        ) : (
+          <>
+            <CustomBtn
+              type="outline"
+              title="AI Logo"
+              customStyles="text-xs"
+              handleClick={() => handleSubmit("logo")}
+            />
+            <CustomBtn
+              type="filled"
+              title="AI Full"
+              customStyles="text-xs"
+              handleClick={() => handleSubmit("full")}
+            />
+          </>
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default AIPicker;
