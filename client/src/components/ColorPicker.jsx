b2f285f0bfb9929ff00551cd588c265b4e774621
Dinara Idrissova <Dinara.Idrissova@list.ru>
2023-10-12 13:04:53 -0400

    File Picker component completed

--- client/src/components/ColorPicker.jsx
+++ client/src/components/ColorPicker.jsx
@@ -1,14 +1,16 @@
 import { SketchPicker } from "react-color";
 import { useSnapshot } from "valtio";
 import state from "../store";
+import { useState } from "react";
 export const ColorPicker = () => {
   const snap = useSnapshot(state);
+
   return (
     <div className="absolute left-full ml-3">
       <SketchPicker
         color={snap.color}
         disableAlpha
-        onChange={(color) => (state.color = color.hex)}
+        onChangeComplete={(color) => (state.color = color.hex)}
       />
     </div>
   );
--- client/src/components/FilePicker.jsx
+++ client/src/components/FilePicker.jsx
@@ -1,5 +1,38 @@
 import React from "react";
+import CustomBtn from "./CustomBtn";
 
-export const FilePicker = () => {
-  return <div>FilePicker</div>;
+export const FilePicker = ({ file, setFile, readFile }) => {
+  return (
+    <div className="filepicker-container">
+      <div className="flex flex-1 flex-col">
+        <input
+          type="file"
+          id="file-upload"
+          accept="image/*"
+          onChange={(e) => setFile(e.target.files[0])}
+        />
+        <label htmlFor="file-upload" className="filepicker-label">
+          Upload File
+        </label>
+        <p className="mt-2 text-gray-500 text-sm truncate">
+          {file === "" ? "No file selected" : file.name}
+        </p>
+      </div>
+
+      <div className="mt-4 flex flex-wrap gap-3">
+        <CustomBtn
+          type="outline"
+          title="Logo"
+          handleClick={() => readFile("logo")}
+          customStyles="text-xs"
+        />
+        <CustomBtn
+          type="filled"
+          title="Full"
+          handleClick={() => readFile("full")}
+          customStyles="text-xs"
+        />
+      </div>
+    </div>
+  );
 };
